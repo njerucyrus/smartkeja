3870fef23074b062c6c61ca171c4dd919e3cf884
njerucyrus <njerucyrus5@gmail.com>
2018-01-17 18:00:01 +0300

    added string amount

--- smarthouse/views.py
+++ smarthouse/views.py
@@ -669,7 +669,7 @@ def ussd_test(request):
                 transactionId = gateway.initiateMobilePaymentCheckout(settings.PRODUCT_NAME,
                                                                       phone_number,
                                                                       settings.CURRENCY_CODE,
-                                                                      float(house.rent_price),
+                                                                      str(house.rent_price),
                                                                       metadata
                                                                       )
                 if transactionId:
